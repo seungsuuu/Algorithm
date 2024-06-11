b2055b2515dd6ef7d86972005ac069a2d86fef02
seungsuuu <48900537+seungsuuu@users.noreply.github.com>
2024-06-11 09:58:33 +0900

    [level 1] Title: 소수 만들기, Time: 0.05 ms, Memory: 83.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        boolean check = true;
+        
+        for(int i=0;i<nums.length;i++){
+            
+            for(int j=i+1;j<nums.length;j++){
+                
+                for(int k=j+1;k<nums.length;k++){
+                    
+                    int num = nums[i]+nums[j]+nums[k];
+                    
+                    answer += isPrime(num) ? 1 : 0;
+                }
+            }
+        }
+        return answer;
+    }
+    
+    private boolean isPrime(int num) {
+		for (int i = 2; i <= Math.sqrt(num); i++){
+			if (num % i == 0){
+				return false;
+			}
+		}
+		return true;
+	} 
+}

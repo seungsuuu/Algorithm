f18420fe2e02b9667bd94644f163872b036470f9
seungsuuu <48900537+seungsuuu@users.noreply.github.com>
2024-06-14 09:21:53 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.17 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int[] count = new int[number + 1];    
+        
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= number / i; j++) {
+                count[i * j]++;
+            }
+        }
+        
+        int answer = 0;
+        for (int k = 1; k <= number; k++) {
+            if (count[k] > limit) {
+                answer += power;
+            } else {
+                answer += count[k];
+            }
+        }
+        return answer;
+    }
+}

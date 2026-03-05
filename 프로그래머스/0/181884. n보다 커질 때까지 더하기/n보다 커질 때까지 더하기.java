b415ed139eb520c38f018412cb24a92932bdc119
seungsuuu <48900537+seungsuuu@users.noreply.github.com>
2026-03-05 22:09:44 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.03 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] numbers, int n) {
+        int answer = 0;
+        
+        for(int number : numbers) {
+            if(answer <= n) {
+                answer += number;
+            } else {
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}

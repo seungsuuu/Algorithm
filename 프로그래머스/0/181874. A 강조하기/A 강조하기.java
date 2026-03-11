96a3ceb0544c13cb6c7d1eda598a36c0857b9f86
seungsuuu <48900537+seungsuuu@users.noreply.github.com>
2026-03-11 15:11:05 +0900

    [level 0] Title: A 강조하기, Time: 13.34 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        
+        for(int i = 0; i < myString.length(); i++) {
+            char c = myString.charAt(i);
+            if(c == 'a') {
+                c = Character.toUpperCase(c);
+            } else if(c != 'A'){
+                c = Character.toLowerCase(c);
+            }
+            answer += c;
+        }
+        return answer;
+    }
+}

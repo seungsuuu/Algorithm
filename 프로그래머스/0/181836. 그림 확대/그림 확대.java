86554c1e9d7a6e49c088b3070c8d4781ee663787
seungsuuu <48900537+seungsuuu@users.noreply.github.com>
2026-03-22 18:11:31 +0900

    [level 0] Title: 그림 확대, Time: 3.96 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] picture, int k) {
+        List<String> list = new ArrayList<>();
+        
+        for (int i = 0; i < picture.length; i++) {
+            String[] enlarge = picture[i].split("");
+            String str = "";
+            for (int j = 0; j < enlarge.length; j++) {
+                str += enlarge[j].repeat(k);
+            }
+            for (int j = 0; j < k; j++) {
+                list.add(str);
+            }
+        }
+        
+        String[] answer = new String[list.size()];
+        list.toArray(answer);
+        
+        return answer;
+    }
+}

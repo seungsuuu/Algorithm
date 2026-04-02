06f6b26ebc3f655be1abc023a326d37fad02fe7f
seungsuuu <48900537+seungsuuu@users.noreply.github.com>
2026-04-02 23:57:19 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.61 ms, Memory: 87.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -0,0 +1,17 @@
+class Solution {
+    public long solution(String numbers) {
+        long answer = 0;
+        
+        String[] matchNumbers = {
+                "zero", "one", "two"
+                , "three", "four", "five"
+                , "six", "seven", "eight", "nine"};
+
+        for (int i = 0; i < matchNumbers.length; i++) {
+            numbers = numbers.replaceAll(matchNumbers[i], String.valueOf(i));
+        }
+        
+        answer = Long.parseLong(numbers);
+        return answer;
+    }
+}

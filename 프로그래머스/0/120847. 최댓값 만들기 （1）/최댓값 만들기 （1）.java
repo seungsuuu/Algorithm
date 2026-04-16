2199a8035ff9b3ee399a92520c3a442e65810759
seungsuuu <48900537+seungsuuu@users.noreply.github.com>
2026-04-16 20:24:14 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 1.26 ms, Memory: 89.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        int length = numbers.length;
+        
+        Arrays.sort(numbers);
+        answer = numbers[length - 1] * numbers[length - 2];
+        
+        return answer;
+    }
+}

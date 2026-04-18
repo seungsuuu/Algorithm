d80fca3dc074788e070581ba2f3335f8e1266a7d
seungsuuu <48900537+seungsuuu@users.noreply.github.com>
2026-04-18 21:25:36 +0900

    [level 0] Title: 가위 바위 보, Time: 3.07 ms, Memory: 82 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String rsp) {
+        String answer = "";
+        
+        String[] rspArr = rsp.split("");
+        for(String s : rspArr) {
+            if(s.equals("2")) {
+                answer += "0";
+            } else if(s.equals("0")) {
+                answer += "5";
+            } else if(s.equals("5")) {
+                answer += "2";
+            }
+        }
+        
+        return answer;
+    }
+}
